b8f666d468caa8ca6c0a9097534bcb4cb27fe335
Haitham <haitham@stretch.com>
2023-01-17 20:59:57 +0400

    small changes

--- app/main.py
+++ app/main.py
@@ -10,22 +10,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-my_posts = [{"title": "title of blog 1", "content": "content of blog 1", "id": 1},
-            {"title": "favourite stretches", "content": "I like child pose", "id": 2}]
-
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
-
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,5 @@
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
@@ -10,11 +10,11 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 # SECRET_KEY
 # Algorithm
-# Expriation time
+# Expiration time
 
 SECRET_KEY = {settings.secret_key}
 ALGORITHM = {settings.algorithm}
-ACCESS_TOKEN_EXPIRE_MINUTES = {settings.access_token_expire_minute}
+ACCESS_TOKEN_EXPIRE_MINUTES = {settings.access_token_expire_minutes}
 
 
 def create_access_token(data: dict):
