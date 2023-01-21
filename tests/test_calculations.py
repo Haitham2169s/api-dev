f7ae5d03699363f8c4d24c29dfd49c0806a3d141
Haitham <haitham@stretch.com>
2023-01-21 17:56:45 +0400

    pytest fixtures

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -2,6 +2,16 @@ from app.calculations import add, subtract, multiply, divide, BankAccount
 import pytest
 
 
+@pytest.fixture
+def zero_bank_account():
+    return BankAccount()
+
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
+
 @pytest.mark.parametrize("num1, num2, expected", [
     (3, 2, 5),
     (7, 1, 8),
@@ -11,34 +21,36 @@ def test_add(num1, num2, expected):
     print("Testing add function")
     assert add(num1, num2) == expected
 
+
 def test_subtract():
     assert subtract(9, 4) == 5
+
 
 def test_multiply():
     assert multiply(5, 8) == 40
 
+
 def test_divide():
     assert divide(81, 9) == 9.0
 
-def test_bank_set_initial_amount():
-    bank_account = BankAccount(50)
+
+def test_bank_set_initial_amount(bank_account):
     assert bank_account.balance == 50
 
-def test_bank_default_amount():
-    bank_account = BankAccount()
-    assert bank_account.balance == 0
+def test_bank_default_amount(zero_bank_account):
+    assert zero_bank_account.balance == 0
 
-def test_bank_withdraw():
-    bank_account = BankAccount(50)
+
+def test_bank_withdraw(bank_account):
     bank_account.withdraw(20)
     assert bank_account.balance == 30
 
-def test_bank_deposit():
-    bank_account = BankAccount(50)
+
+def test_bank_deposit(bank_account):
     bank_account.deposit(30)
     assert bank_account.balance == 80
 
-def test_collect_interest():
-    bank_account = BankAccount(50)
+
+def test_collect_interest(bank_account):
     bank_account.collect_interest()
     assert round(bank_account.balance, 6) == 55
