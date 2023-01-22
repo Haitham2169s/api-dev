32acc8c7f28982d5b9565cfd1fb2d74c23d0fca3
Haitham <haitham@stretch.com>
2023-01-22 10:07:16 +0400

    pytest get all post with authentication

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -11,4 +11,19 @@ def test_get_all_posts(authorized_client, test_posts):
 
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
-    assert posts_list[0].Post.id == test_posts[0].id
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+def test_get_one_post_not_exist(authorized_client, test_posts):
+    res = authorized_client.get("/posts/88888")
+    assert res.status_code == 404
+
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+    print(res.json())
+    post = schemas.PostOut(**res.json())
+    assert post.Post.id == test_posts[0].id
+    assert post.Post.content == test_posts[0].content
+    assert post.Post.title == test_posts[0].title
