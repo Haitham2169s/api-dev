2a0dbb898afdbb7ad3090e72075646664befb725
Haitham <haitham@stretch.com>
2023-01-22 08:39:18 +0400

    parametrize multiple login details

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,20 +1,7 @@
 import pytest
 from jose import jwt
 from app import schemas
-from .database import client, session
 from app.config import settings
-
-@pytest.fixture
-def test_user(client):
-    user_data = {"email": "haitham@stretch.com", "password": "password123"}
-    res = client.post("/users/", json=user_data)
-
-    assert res.status_code == 201
-    print(res.json)
-
-    new_user = res.json()
-    new_user["password"] = user_data["password"]
-    return new_user
 
 
 # def test_root(client):
@@ -41,3 +28,16 @@ def test_login_user(test_user, client):
     assert id == test_user["id"]
     assert login_res.token_type == 'bearer'
     assert res.status_code == 200
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ("wrongemail@gmail.com", "password123", 403),
+    ("haitham@stretch.com", "wrongpassword", 403),
+    ("wrongemail@gmail.com", "password123", 403),
+    (None, "password123", 422),
+    ("haitham@stretch.com", None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    # assert res.json().get("detail") == "Invalid Credentials"
